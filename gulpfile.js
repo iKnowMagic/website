5a23682c6a27fe16d4ac17e6e86700ed04081bc8
Ilan <ilan@iknowmagic.net>
2016-12-01 21:00:51 +0000

    gulp work

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 var del = require('del');
-
 var runSequence = require('run-sequence');
 var lazypipe = require('lazypipe');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 var options = {
   dev: 'dev',
@@ -27,8 +28,21 @@ gulp.task('fonts', function() {
     .pipe(gulp.dest(options.dist + '/fonts/'));
 });
 
-gulp.task('html', ['img', 'fonts'], function() {
+gulp.task('clean', del.bind(null, [options.tmp, options.dist]));
 
+gulp.task('inject', function() {
+  gulp.src(options.dev + '/index.html')
+  .pipe($.inject(gulp.src([
+    options.dev + '/partials/**/*.js',
+    options.tmp + '/partials/**/*.js',
+  ], {read: false}), {relative: true}))
+  .pipe($.tap(function(file, t) {
+    file.contents = new Buffer(file.contents.toString().replace(/\.\.\/\.tmp\//g, ''));
+  }))
+  .pipe(gulp.dest(options.tmp));
+});
+
+gulp.task('html', ['img', 'fonts'], function() {  
   return gulp.src(options.tmp + '/*.html')
     .pipe($.plumber())
     .pipe($.eol())
@@ -36,14 +50,72 @@ gulp.task('html', ['img', 'fonts'], function() {
       searchPath: ['.', options.dev, options.tmp],
     },
     lazypipe().pipe($.sourcemaps.init, { loadMaps: true })))
-
+  
     // .pipe($.if('*/main.js', $.ngAnnotate()))
     // .pipe($.if('*/main.js', $.uglify())) //$.uglify({mangle: false})
 
-    .pipe($.if(/^((?!config\.js).)*\.js$/, $.rev())) //do not use rev() on config.js
+    .pipe($.if('*.js', $.rev()))
     .pipe($.if('*.css', $.rev()))
     .pipe($.revReplace())
 
     .pipe($.sourcemaps.write('maps'))
     .pipe(gulp.dest(options.dist));
+});
+
+gulp.task('build', function(callback) {
+  runSequence(
+    'clean',
+    'inject',
+    'html',
+    callback
+  );
+});
+
+gulp.task('serve-dev', ['build'], function() {
+  browserSync({
+    //notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['local_files', '.tmp', 'dev'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    },
+    open: false
+  });
+
+  gulp.watch([
+    options.dev + '/partials/**/*',
+    options.dev + '/scss/**/*.scss',
+    options.dev + '/scss/**/**/*.scss',
+    options.dev + '/*.html'
+  ], ['build']
+  );
+
+  gulp.watch(options.dev + '/img/**/*', ['img']).on('change', reload);
+  gulp.watch(options.dev + '/fonts/**/*', ['fonts']).on('change', reload);
+
+  gulp.watch(options.local + '/**/*').on('change', reload);
+});
+
+gulp.task('serve', ['build'], function() {
+
+  browserSync({
+    //notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['app']
+    },
+    open: false
+  });
+
+  gulp.watch([
+    options.dev + '/scss/**/*.scss',
+    options.dev + '/partials/**/*',
+    options.dev + '/*.html'
+  ], ['build']
+  );
+
+  gulp.watch(options.dev + '/img/**/*', ['img']).on('change', reload);
+  gulp.watch(options.dev + '/fonts/**/*', ['fonts']).on('change', reload);
 });
