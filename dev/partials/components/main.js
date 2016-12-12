c0fcdc6c803183d86358c43284d7efe59ead8914
iknowmagic <ilan@iknowmagic.com>
2016-12-12 00:18:18 -0800

    fx

--- dev/partials/components/main.js
+++ dev/partials/components/main.js
@@ -1,9 +1,8 @@
 Vue.component('main-component', {
-  template: '<div>{{message}}</div>',
+  template: '<h1>{{message}}</h1>',
   data: function() {
     return {
       message: 'All I see is magic.'
     }
   }
 });
-
